c6d9c1cf92e61e7ae802e5af7e3dba5288e9aaab
Petra-Johansson <johansson.m.petra@gmail.com>
2025-04-23 21:59:38 +0200

    DeckOfCards-class created

---
+++ src/Card/DeckOfCards.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Card;
+
+use App\Card\Card;
+
+class DeckOfCards
+{
+    private array $cards = [];
+
+    public function __construct()
+    {
+        $this->generateDeck();
+    }
+
+    private function generateDeck(): void
+    {
+        $suits = ['Spades', 'Hearts', 'Clubs', 'Diamonds'];
+        $values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+
+        foreach ($suits as $suit) {
+            foreach ($values as $value) {
+                $this->$cards[] = new Card($suit, $value);
+            }
+        }
+    }
+
+    public function shuffle(): void
+    {
+        shuffle($this->cards);
+    }
+
+    public function drawCard(): ?Card
+    {
+        return array_pop($this->cards);
+    }
+
+    public function drawCards(int $number): array
+    {
+        $drawn = [];
+        for ($i = 0; $i < $number && !empty($this->cards); $i++) {
+            $drawn[] = $this->drawCard();
+        }
+        return $drawn;
+    }
+
+    public function getCards(): array
+    {
+        return $this->cards;
+    }
+}
